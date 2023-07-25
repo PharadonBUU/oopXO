fc53884b04bc1ce5aa3ab704bdcfc055664c9677
Pharadon Sirijhan <64160142@go.buu.ac.th>
2023-07-25 15:42:05 +0700

    Draw Function

--- src/main/java/com/mycompany/lab4/Table.java
+++ src/main/java/com/mycompany/lab4/Table.java
@@ -12,7 +12,7 @@ public class Table {
 
     private char[] table = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
     private Player player1, player2, currentPlayer;
-
+    private int count;
     public Table(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
@@ -33,12 +33,17 @@ public class Table {
             return false;
         } else {
             table[position - 1] = currentPlayer.getSymbol();
+            this.count++;
             return true;
         }
 
     }
 
     public boolean checkWin() {
+        if (checkRow1()) {
+            return true;
+        }
+        saveWin();
         return false;
     }
 
@@ -49,7 +54,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkRow2() {
         if (table[3] == currentPlayer.getSymbol() && table[4] == currentPlayer.getSymbol() && table[5] == currentPlayer.getSymbol()) {
             return true;
@@ -57,7 +62,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkRow3() {
         if (table[6] == currentPlayer.getSymbol() && table[7] == currentPlayer.getSymbol() && table[8] == currentPlayer.getSymbol()) {
             return true;
@@ -65,7 +70,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkCol1() {
         if (table[0] == currentPlayer.getSymbol() && table[3] == currentPlayer.getSymbol() && table[6] == currentPlayer.getSymbol()) {
             return true;
@@ -73,7 +78,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkCol2() {
         if (table[1] == currentPlayer.getSymbol() && table[4] == currentPlayer.getSymbol() && table[7] == currentPlayer.getSymbol()) {
             return true;
@@ -81,7 +86,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkCol3() {
         if (table[2] == currentPlayer.getSymbol() && table[5] == currentPlayer.getSymbol() && table[8] == currentPlayer.getSymbol()) {
             return true;
@@ -89,7 +94,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkDiagonalLeft() {
         if (table[0] == currentPlayer.getSymbol() && table[4] == currentPlayer.getSymbol() && table[8] == currentPlayer.getSymbol()) {
             return true;
@@ -97,7 +102,7 @@ public class Table {
             return false;
         }
     }
-    
+
     private boolean checkDiagonalRight() {
         if (table[2] == currentPlayer.getSymbol() && table[4] == currentPlayer.getSymbol() && table[6] == currentPlayer.getSymbol()) {
             return true;
@@ -105,6 +110,25 @@ public class Table {
             return false;
         }
     }
+
+    private void saveWin() {
+        if (currentPlayer == player1) {
+            player1.win();
+            player2.lose();
+        } else {
+            player2.win();
+            player1.lose();
+        }
+    }
     
-    
+    public boolean checkDraw() {
+        if(count==9) {
+            player1.draw();
+            player2.draw();
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
